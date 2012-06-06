e97f2527cf6ee0423b4127e1e62f5a2ebebb912d
Osamu Noguchi <noguchi999@gmail.com>
2012-06-06 19:28:48 +0900

    remove config files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Runpage::Application.config.secret_token = '331f6e217999c0ce72ddee59a15966f98765c9fd5625b69fdf6507c0844c6b9ffbf4334d7d308c95be7d0c19bd086f95bedb239381601cecb7130b922b200b13'
